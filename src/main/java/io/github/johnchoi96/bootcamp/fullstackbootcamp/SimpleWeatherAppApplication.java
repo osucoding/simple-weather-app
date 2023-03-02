cca7bba1f8175d7fa317e3075cdc52eaf6e88333
John Choi <johnchoi1003@icloud.com>
2023-03-01 22:04:51 -0500

    first commit

---
+++ src/main/java/io/github/johnchoi96/bootcamp/fullstackbootcamp/SimpleWeatherAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.johnchoi96.bootcamp.fullstackbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWeatherAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWeatherAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/johnchoi96/bootcamp/fullstackbootcamp/SimpleWeatherAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.johnchoi96.bootcamp.fullstackbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWeatherAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
