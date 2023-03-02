5556496374dcdc5e5a86b30338150f8d8a9569d5
John Choi <johnchoi1003@icloud.com>
2023-03-02 10:56:30 -0500

    Cors origin policy added

--- backend/src/main/java/io/github/johnchoi96/bootcamp/fullstackbootcamp/SimpleWeatherAppApplication.java
+++ backend/src/main/java/io/github/johnchoi96/bootcamp/fullstackbootcamp/SimpleWeatherAppApplication.java
@@ -2,12 +2,25 @@ package io.github.johnchoi96.bootcamp.fullstackbootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SimpleWeatherAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleWeatherAppApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
