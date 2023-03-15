d045edf13a49335b04b6d815524f61f37e9dad6e
John Choi <johnchoi1003@icloud.com>
2023-03-15 00:55:32 -0400

    Controller return type to ResponseEntity

--- backend/src/main/java/io/github/johnchoi96/bootcamp/fullstackbootcamp/controllers/SampleController.java
+++ backend/src/main/java/io/github/johnchoi96/bootcamp/fullstackbootcamp/controllers/SampleController.java
@@ -5,6 +5,7 @@ import io.github.johnchoi96.bootcamp.fullstackbootcamp.client.SampleClient;
 import io.github.johnchoi96.bootcamp.fullstackbootcamp.models.NyTimesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,17 +19,17 @@ public class SampleController {
     private SampleClient client;
 
     @GetMapping(value = "/sayHi", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String getHelloWorld() {
-        return "hello world";
+    public ResponseEntity<String> getHelloWorld() {
+        return ResponseEntity.ok("hello world");
     }
 
     @GetMapping(value = "/sayMyName", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String sayHelloWithMyName(@RequestParam("name") final String name) {
-        return "hello " + name + "!";
+    public ResponseEntity<String> sayHelloWithMyName(@RequestParam("name") final String name) {
+        return ResponseEntity.ok("hello " + name + "!");
     }
 
     @GetMapping(value = "/nyTimes", produces = MediaType.APPLICATION_JSON_VALUE)
-    public NyTimesResponse getWeather(@RequestParam("sectionValue") final String sectionValue) throws JsonProcessingException {
-        return client.getNews(sectionValue);
+    public ResponseEntity<NyTimesResponse> getWeather(@RequestParam("sectionValue") final String sectionValue) throws JsonProcessingException {
+        return ResponseEntity.ok(client.getNews(sectionValue));
     }
 }
